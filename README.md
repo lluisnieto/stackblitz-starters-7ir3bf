085af9743baff22d574bc63d8b9e8476ccb40fe7
lluisnieto <anconetano@gmail.com>
2024-10-24 10:09:31 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# stackblitz-starters-7ir3bf
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/lluisnieto/stackblitz-starters-7ir3bf)
---
+++ src/main.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { AutocompleteComponent } from 'components/autocomplete';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  template: `
+    <app-autocomplete></app-autocomplete>
+  `,
+  imports: [AutocompleteComponent],
+})
+export class App {
+  name = 'Angular';
+}
+
+bootstrapApplication(App);
